91a4c6245db939ff9d1b489ebb7a31c5bfc41c15
Makers <makers@Makerss-MacBook-Air.local>
2016-05-24 14:50:16 +0100

    Created dock method

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,13 @@
 class DockingStation
+  attr_reader :docked_bike
+  def initialize
+    @docked_bike
+  end
   def release_bike
     Bike.new
+  end
+  def dock (bike)
+    @docked_bike = bike
   end
 end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,6 +2,20 @@ require 'docking_station'
 
 describe DockingStation do
   it {subject.release_bike}
+  describe "dock" do
+    it "expects dock method to exist" do
+
+      expect(subject.dock(Bike.new))
+    end
+
+  it "expects a value in doc" do
+    station = DockingStation.new
+    testbike = station.release_bike
+    station.dock(testbike)
+    expect(station.docked_bike).to eq testbike
+  end
+
+  end
 end
 
 describe Bike do
