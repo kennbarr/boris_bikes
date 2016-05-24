713248bdf94c672d87e7fd83d09c4776d8a9c3b0
Kenneth Barrett <barrett.kenneth@gmail.com>
2016-05-24 14:08:48 +0100

    Docking station now creates bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,3 +3,9 @@ require 'docking_station'
 describe DockingStation do
   it {subject.release_bike}
 end
+
+describe Bike do
+  it "Expects the bike to be working" do
+    expect(subject.working?).to eq true
+  end
+end
